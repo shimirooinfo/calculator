4b5d80219f52b93c51f49830d9906958fe608aa4
shimirooinfo <shim@iroo.co.kr>
2023-06-13 13:33:56 +0900

    Modified ui.py

--- ctrl.py
+++
@@ -1,8 +0,0 @@
-class Control:
-    def __init__(self, view):
-        self.view = view
-        self.connectSignals()
-        
-    def connectSignals(self):
-        self.view.btn1.clicked.connect(self.view.activateMessage)
-        self.view.btn2.clicked.connect(self.view.clearMessage)
